619d60249e8ab6e8a213c1c75eab52ab744f227a
Haojie Zhang <haojie.bit@gmail.com>
2018-01-11 08:52:08 +0800

    add variable

--- src/listener.cpp
+++ src/listener.cpp
@@ -60,6 +60,8 @@ int main(int argc, char **argv)
    */
   ros::NodeHandle n;
 
+  int i;
+
   /**
    * The subscribe() call is how you tell ROS that you want to receive messages
    * on a given topic.  This invokes a call to the ROS
